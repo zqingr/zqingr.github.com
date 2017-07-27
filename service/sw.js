96f5f7b2e6c98ff9cee7b5ff792fc9eed2073b00
qing-ren-zi <qing-ren.zi@hpe.com>
2017-07-27 15:50:59 +0800

    up

--- service/sw.js
+++ service/sw.js
@@ -16,6 +16,7 @@ self.addEventListener('install', function(event) {
 self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
+      console.log(cacheNames)
       return Promise.all(
         cacheNames.map(function(cacheName) {
           // 如果当前版本和缓存版本不一致
